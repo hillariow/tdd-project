0d41e9f1689c82f420f2de5ac129dd76c0359bca
hillariow <mateushilariolima@gmail.com>
2021-08-13 19:43:31 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 5

--- lists/views.py
+++ lists/views.py
@@ -16,3 +16,6 @@ def view_list(request):
 def new_list(request):
     Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/')
+
+def home_page(request):
+    return render(request, 'home.html')
